378f181c0b9eda1db8086009a6b3edc89a395903
Mike Haller <info@mhaller.de>
2010-03-11 19:08:14 +0000

    Test for larger reports

--- src/main/java/org/sonar/plugins/clirr/ClirrTxtResultParser.java
+++ src/main/java/org/sonar/plugins/clirr/ClirrTxtResultParser.java
@@ -4,6 +4,8 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,17 +13,22 @@ import java.util.regex.Pattern;
 
 public final class ClirrTxtResultParser {
 
-  public List<ClirrViolation> parse(final InputStream input, final Charset charset) throws IOException {
-    List<ClirrViolation> violations = new ArrayList<ClirrViolation>();
-    List<String> lines = IOUtils.readLines(input, charset.name());
-    for (String line : lines) {
-      String[] split = line.split(Pattern.quote(":"));
-      if (split.length >= 4) {
-        violations.add(parseViolationLine(split));
-      }
-    }
-    return violations;
-  }
+	public List<ClirrViolation> parse(final InputStream input, final Charset charset)
+			throws IOException {
+		return parse(new InputStreamReader(input, charset));
+	}
+
+	public List<ClirrViolation> parse(final Reader input) throws IOException {
+		List<ClirrViolation> violations = new ArrayList<ClirrViolation>();
+		List<String> lines = IOUtils.readLines(input);
+		for (String line : lines) {
+			String[] split = line.split(Pattern.quote(":"));
+			if (split.length >= 4) {
+				violations.add(parseViolationLine(split));
+			}
+		}
+		return violations;
+	}
 
   private ClirrViolation parseViolationLine(String[] split) {
     return new ClirrViolation(split[0].trim(), split[1].trim(), split[3].trim(), split[2].trim());
--- src/test/java/org/sonar/plugins/clirr/ClirrTxtResultParserTest.java
+++ src/test/java/org/sonar/plugins/clirr/ClirrTxtResultParserTest.java
@@ -1,24 +1,60 @@
 package org.sonar.plugins.clirr;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-import org.sonar.api.resources.JavaFile;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.nio.charset.Charset;
 import java.util.List;
 
+import org.junit.Test;
+import org.sonar.api.resources.JavaFile;
+
 public class ClirrTxtResultParserTest {
 
-  @Test
-  public void parseResult() throws Exception {
-    ClirrTxtResultParser clirrParser = new ClirrTxtResultParser();
-    List<ClirrViolation> violations = clirrParser.parse(ClirrTxtResultParserTest.class
-        .getResourceAsStream("/clirr-report.txt"), Charset.defaultCharset());
-    assertEquals(106, violations.size());
-    assertEquals("ERROR", violations.get(0).getSeverity());
-    assertEquals("8001", violations.get(0).getMessageId());
-    assertEquals(new JavaFile("org.sonar.plugins.api.AbstractLanguage"), violations.get(0).getJavaFile());
-    assertEquals("Class org.sonar.plugins.api.AbstractLanguage removed", violations.get(0).getMessage());
-  }
+	private static class RepeatingStringReader extends StringReader {
+
+		private int repeats;
+
+		public RepeatingStringReader(String s, int repeats) {
+			super(s);
+			this.repeats = repeats;
+		}
+
+		@Override
+		public int read(char[] cbuf, int off, int len) throws IOException {
+			int read = super.read(cbuf, off, len);
+			if (read == -1 && --repeats > 0) {
+				reset();
+				return super.read(cbuf, off, len);
+			}
+			return read;
+		}
+
+	}
+
+	@Test
+	public void parseResult() throws Exception {
+		ClirrTxtResultParser clirrParser = new ClirrTxtResultParser();
+		List<ClirrViolation> violations = clirrParser.parse(ClirrTxtResultParserTest.class
+				.getResourceAsStream("/clirr-report.txt"), Charset.defaultCharset());
+		assertEquals(106, violations.size());
+		assertEquals("ERROR", violations.get(0).getSeverity());
+		assertEquals("8001", violations.get(0).getMessageId());
+		assertEquals(new JavaFile("org.sonar.plugins.api.AbstractLanguage"), violations.get(0)
+				.getJavaFile());
+		assertEquals("Class org.sonar.plugins.api.AbstractLanguage removed", violations.get(0)
+				.getMessage());
+	}
+
+	@Test
+	public void largeReport() throws Exception {
+		final int repeats = 10000;
+		final String repeatedLine = "ERROR: 8001: org.sonar.plugins.api.AbstractLanguage: Class org.sonar.plugins.api.AbstractLanguage removed\n";
+		ClirrTxtResultParser clirrParser = new ClirrTxtResultParser();
+		StringReader reader = new RepeatingStringReader(repeatedLine, repeats);
+		List<ClirrViolation> violations = clirrParser.parse(reader);
+		assertEquals(repeats, violations.size());
+	}
 
 }
