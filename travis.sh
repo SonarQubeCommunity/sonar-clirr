0b5bf664386cfdda01f99add41c7eaa168678283
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-06-09 23:14:31 +0200

    Fix Travis build

--- travis.sh
+++ travis.sh
@@ -4,69 +4,20 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v28 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
-function strongEcho {
-  echo ""
-  echo "================ $1 ================="
-}
+mvn install -B -e -V
 
-case "$TARGET" in
+# execute integration tests
 
-CI)
-  if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-    strongEcho 'Build and analyze commit in master'
-    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
-      -Pcoverage-per-test \
-      -Dmaven.test.redirectTestOutputToFile=false \
-      -Dsonar.host.url=$SONAR_HOST_URL \
-      -Dsonar.login=$SONAR_TOKEN \
-      -B -e -V
+# SonarQube 5.6 LTS requires Java 8
+jdk_switcher use oraclejdk8
 
-  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-    strongEcho 'Build and analyze pull request'
-    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
-      -Pcoverage-per-test \
-      -Dmaven.test.redirectTestOutputToFile=false \
-      -Dsonar.analysis.mode=issues \
-      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-      -Dsonar.github.oauth=$GITHUB_TOKEN \
-      -Dsonar.host.url=$SONAR_HOST_URL \
-      -Dsonar.login=$SONAR_TOKEN \
-      -B -e -V
+# enable xvfb for selenium tests
+installTravisTools
+start_xvfb
 
-  else
-    strongEcho 'Build, no analysis'
-    # Build branch, without any analysis
-
-    # No need for Maven goal "install" as the generated JAR file does not need to be installed
-    # in Maven local repository
-    mvn verify -Dmaven.test.redirectTestOutputToFile=false -B -e -V
-  fi
-
-  ;;
-
-IT)
-  installTravisTools
-  start_xvfb
-
-  ./quick-build.sh
-
-  if [ "${SQ_VERSION}" == "DEV" ];
-  then
-    build_snapshot "SonarSource/sonarqube"
-  fi
-
-  cd its
-  mvn -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false verify
-  ;;
-
-*)
-  echo "Unexpected TARGET value: $TARGET"
-  exit 1
-  ;;
-
-esac
+cd its
+mvn -Dsonar.runtimeVersion="LTS" -Dmaven.test.redirectTestOutputToFile=false verify
